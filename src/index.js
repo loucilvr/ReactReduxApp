0ad81909df10bac3423a080b20d90c9c761c8ec8
Loucil Rubio <mvr12@my.fsu.edu>
2017-07-31 23:40:06 -0400

    added searchbar

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBZ-vOKp3Uf_OO0F0fQHe7IquD1mQ-XpmA';
 
 const App = function(){
-    return <div>Hi!</div>;
+    return <div>
+            <SearchBar/>
+            </div>;
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
