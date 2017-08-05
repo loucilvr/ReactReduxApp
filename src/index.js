f5db586bfd60d285f7381f9769642f2867686f81
Loucil Rubio <mvr12@my.fsu.edu>
2017-08-05 01:51:56 -0400

    display youtube video with details and a list of searched videos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 /**
  * Created by LoucilRubio on 7/19/17.
  */
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
 const API_KEY = 'AIzaSyBZ-vOKp3Uf_OO0F0fQHe7IquD1mQ-XpmA';
 
-const App = function(){
-    return <div>
-            <SearchBar/>
-            </div>;
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {videos : []};
+        YTSearch({key : API_KEY, term: 'lawf'}, videos => this.setState({ videos }));
+        // YTSearch({key : API_KEY, term: 'lawf'}, videos => { console.log(videos)});
+    }
+
+     render() {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoDetail video={this.state.videos[0]}/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
